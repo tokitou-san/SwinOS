b01492b13963c791808644897ea135bc7a396e36
tokitou-san <tokitousan2005@gmail.com>
2023-07-28 13:29:18 +0530

    style: format codes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ![Desktop Welcome window](https://github.com/tokitou-san/SwinOS/assets/114811070/7afce567-81fc-48cf-904d-2b361a86d291)
 
 # Swin OS
+
 A super cool OS like thingy on web powered by Svelte
+
 > Not in active development
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
     import "../app.css";
 </script>
 
-<main style="background-image: url(/images/wallpaper.jpg);" class="w-screen h-screen bg-cover bg-no-repeat">
+<main
+    style="background-image: url(/images/wallpaper.jpg);"
+    class="h-screen w-screen bg-cover bg-no-repeat"
+>
     <slot />
 </main>
