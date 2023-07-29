7455ab68598d70c3a7623e521a859328f3dfed45
tokitou-san <tokitousan2005@gmail.com>
2023-07-29 22:56:03 +0530

    style: add `herf` for github icon

--- src/lib/components/topbar.svelte
+++ src/lib/components/topbar.svelte
@@ -4,7 +4,7 @@
 
 <topbar class="w-full text-white flex items-center justify-between px-2 leading-none h-10 bg-white/10 backdrop-blur-xl block">
 	<github-icon>
-		<a href="/" class="opacity-75 hover:opacity-100 transition-opacity">
+		<a href="https://github.com/tokitou-san/SwinOS" target="_blank" class="opacity-75 hover:opacity-100 transition-opacity">
 			<Github class="w-6" />
 		</a>
 	</github-icon>
