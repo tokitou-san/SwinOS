7c194bfea4a45e31fcdf20c4054b0b8257d8f17c
tokitou-san <tokitousan2005@gmail.com>
2023-07-29 22:48:19 +0530

    style: add dummy topbar elements

--- src/lib/components/topbar.svelte
+++ src/lib/components/topbar.svelte
@@ -1,3 +1,11 @@
-<topbar class="w-full h-10 bg-white/10 backdrop-blur-xl block">
-	
+<topbar class="w-full text-white flex items-center justify-between px-5 leading-none h-10 bg-white/10 backdrop-blur-xl block">
+	<github-icon>
+		Github
+	</github-icon>
+	<time>
+		Time
+	</time>
+	<controls>
+		Controls
+	</controls>
 </topbar>
