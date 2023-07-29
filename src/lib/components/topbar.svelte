a0eb7addd0c5e667c64707a5f23804ae58890dab
tokitou-san <tokitousan2005@gmail.com>
2023-07-29 23:17:53 +0530

    feat: add `current_date_time` function

--- src/lib/components/topbar.svelte
+++ src/lib/components/topbar.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
     import Github from "$lib/icons/github.svelte";
+
+    function get_time_date(): string {
+        let current_time = new Date();
+
+        let current_month = current_time.toLocaleString("en-US", {month: "short"});
+        let current_day = current_time.getDate();
+        let current_hour_minute = current_time.toLocaleTimeString("en-US", { hour12: true, hour: "numeric", minute: "numeric" });
+
+        return `${current_month} ${current_day} ${current_hour_minute}`;
+    }
 </script>
 
 <topbar class="block flex h-10 w-full items-center justify-between bg-white/10 px-2 leading-none text-white backdrop-blur-xl">
@@ -12,6 +22,6 @@
             <Github class="w-6" />
         </a>
     </github-icon>
-    <time>Time</time>
+    <time>{get_time_date()}</time>
     <controls>Controls</controls>
 </topbar>
