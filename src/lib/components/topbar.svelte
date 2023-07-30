b3abc208695deca9a634263c4ca56d9efe3332f6
tokitou-san <tokitousan2005@gmail.com>
2023-07-30 18:11:09 +0530

    feat: add toggle controls popdown

--- src/lib/components/topbar.svelte
+++ src/lib/components/topbar.svelte
@@ -10,6 +10,7 @@
     import Settings from "$lib/icons/settings.svelte";
     import Volume from "$lib/icons/volume.svelte";
     import Wifi from "$lib/icons/wifi.svelte";
+    import { toggle_class } from "svelte/internal";
     // import { onMount } from "svelte";
     import { fly } from "svelte/transition";
 
@@ -32,6 +33,12 @@
     //     let date_time_interval = setInterval(get_time_date(), 1000);
     //     return () => clearInterval(date_time_interval);
     // });
+
+    /* Show controls */
+    let show_controls_popdown = false;
+    function toggle_controls_popdown() {
+        show_controls_popdown = !show_controls_popdown;
+    }
 </script>
 
 <topbar
@@ -50,7 +57,10 @@
         <span class="text-xs font-semibold">{current_time_date}</span>
     </time>
     <controls class="relative mr-2 h-3/4 cursor-pointer">
-        <controls-trigger class="flex h-full items-center gap-2 rounded-md px-2 transition-colors hover:bg-white/10">
+        <controls-trigger
+            class="flex h-full items-center gap-2 rounded-md px-2 transition-colors hover:bg-white/10"
+            on:mousedown={toggle_controls_popdown}
+        >
             <volume>
                 <Volume class="w-4" />
             </volume>
@@ -66,64 +76,70 @@
             </power>
         </controls-trigger>
 
-        <controls-popdown class="absolute right-0 flex h-max w-72 translate-y-3 transform flex-col gap-4 rounded-md bg-white/10 p-3 backdrop-blur-xl">
-            <system-control-buttons class="flex items-center justify-between">
-                <battery class="flex items-center gap-1">
-                    <Battery_80 class="w-4" />
-                    <span class="select-none text-xs font-semibold">80%</span>
-                </battery>
+        {#if show_controls_popdown}
+            <controls-popdown
+                in:fly={{ y: -5 }}
+                out:fly={{ y: -5 }}
+                class="absolute right-0 flex h-max w-72 translate-y-3 transform flex-col gap-4 rounded-md bg-white/10 p-3 backdrop-blur-xl"
+            >
+                <system-control-buttons class="flex items-center justify-between">
+                    <battery class="flex items-center gap-1">
+                        <Battery_80 class="w-4" />
+                        <span class="select-none text-xs font-semibold">80%</span>
+                    </battery>
 
-                <other-controls class="flex items-center gap-2">
-                    <settings class="rounded-full bg-white/10 p-2 transition-colors hover:bg-white/20 active:bg-white/25">
-                        <Settings class="w-4" />
-                    </settings>
-                    <lockscreen class="rounded-full bg-white/10 p-2 transition-colors hover:bg-white/20 active:bg-white/25">
-                        <Lock class="w-4" />
-                    </lockscreen>
-                    <power class="rounded-full bg-white/10 p-2 transition-colors hover:bg-white/20 active:bg-white/25">
-                        <Power class="w-4" />
-                    </power>
-                </other-controls>
-            </system-control-buttons>
+                    <other-controls class="flex items-center gap-2">
+                        <settings class="rounded-full bg-white/10 p-2 transition-colors hover:bg-white/20 active:bg-white/25">
+                            <Settings class="w-4" />
+                        </settings>
+                        <lockscreen class="rounded-full bg-white/10 p-2 transition-colors hover:bg-white/20 active:bg-white/25">
+                            <Lock class="w-4" />
+                        </lockscreen>
+                        <power class="rounded-full bg-white/10 p-2 transition-colors hover:bg-white/20 active:bg-white/25">
+                            <Power class="w-4" />
+                        </power>
+                    </other-controls>
+                </system-control-buttons>
 
-            <volume-brightness-controls class="flex flex-col gap-3">
-                <volume class="flex items-center gap-3">
-                    <Volume class="w-4" />
-                    <input
-                        type="range"
-                        value="65"
-                        class="h-1 w-full cursor-pointer rounded-lg bg-white/20"
-                    />
-                </volume>
-                <brightness class="flex items-center gap-3">
-                    <Brightness class="w-4" />
-                    <input
-                        type="range"
-                        value="10"
-                        class="h-1 w-full cursor-pointer rounded-lg bg-white/20"
-                    />
-                </brightness>
-            </volume-brightness-controls>
+                <volume-brightness-controls class="flex flex-col gap-3">
+                    <volume class="flex items-center gap-3">
+                        <Volume class="w-4" />
+                        <input
+                            type="range"
+                            value="65"
+                            class="h-1 w-full cursor-pointer rounded-lg bg-white/20"
+                        />
+                    </volume>
+                    <brightness class="flex items-center gap-3">
+                        <Brightness class="w-4" />
+                        <input
+                            type="range"
+                            value="10"
+                            class="h-1 w-full cursor-pointer rounded-lg bg-white/20"
+                        />
+                    </brightness>
+                </volume-brightness-controls>
 
-            <connectivity-controls class="grid grid-cols-2 gap-2">
-                <wifi class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
-                    <Wifi class="w-4" />
-                    <span class="text-xs font-semibold">Wifi</span>
-                </wifi>
-                <bluetooth class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
-                    <Bluetooth class="w-4" />
-                    <span class="text-xs font-semibold">Bluetooth</span>
-                </bluetooth>
-                <night-light class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
-                    <Night class="w-4" />
-                    <span class="text-xs font-semibold">Night Light</span>
-                </night-light>
-                <airplane-mode class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
-                    <Airplane class="w-4" />
-                    <span class="text-xs font-semibold">Airplane</span>
-                </airplane-mode>
-            </connectivity-controls>
-        </controls-popdown>
+                <connectivity-controls class="grid grid-cols-2 gap-2">
+                    <wifi class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
+                        <Wifi class="w-4" />
+                        <span class="text-xs font-semibold">Wifi</span>
+                    </wifi>
+                    <bluetooth class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
+                        <Bluetooth class="w-4" />
+                        <span class="text-xs font-semibold">Bluetooth</span>
+                    </bluetooth>
+                    <night-light class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
+                        <Night class="w-4" />
+                        <span class="text-xs font-semibold">Night Light</span>
+                    </night-light>
+                    <airplane-mode class="flex h-10 select-none items-center gap-2 rounded-md bg-white/10 pl-3 transition-colors hover:bg-white/20 active:bg-white/25">
+                        <Airplane class="w-4" />
+                        <span class="text-xs font-semibold">Airplane</span>
+                    </airplane-mode>
+                </connectivity-controls>
+            </controls-popdown>
+        {/if}
     </controls>
 </topbar>
 
