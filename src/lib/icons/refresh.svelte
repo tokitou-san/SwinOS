775ef0e48c7f0f8346cda656529b8127353eb174
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 05:02:01 +0530

    Create refresh.svelte

---
+++ src/lib/icons/refresh.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import type { SVGAttributes } from "svelte/elements";
+    type $$Props = SVGAttributes<SVGElement>;
+</script>
+
+<svg
+    {...$$props}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+>
+    <g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+    >
+        <path d="M21.888 13.5C21.164 18.311 17.013 22 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2c4.1 0 7.625 2.468 9.168 6" />
+        <path d="M17 8h4.4a.6.6 0 0 0 .6-.6V3" />
+    </g>
+</svg>
