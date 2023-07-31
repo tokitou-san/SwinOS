641c6e42707d5c8b1167045ebb1d686e59b6bd95
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 03:16:30 +0530

    feat: add basic section for main context menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import MainContextMenu from "$lib/components/contextmenus/main_context_menu.svelte";
     import Topbar from "$lib/components/topbar.svelte";
 
     const SHOW_COMPONENTS_DELAY = 300;
@@ -9,36 +8,20 @@
     setTimeout(() => {
         show_topbar = true;
     }, SHOW_COMPONENTS_DELAY);
-
-    /* Context menu */
-    let show_main_context_menu = false;
-    let context_pos = { x: 0, y: 0 };
-
-    function handle_right_click(event: MouseEvent) {
-        // show/hide custom menu
-        show_main_context_menu = !show_main_context_menu;
-        // set event position
-        context_pos = { x: event.clientX, y: event.clientY };
-    }
-    function close_context_menu() {
-        show_main_context_menu = false;
-    }
 </script>
 
 <svelte:head>
     <title>Swin OS</title>
 </svelte:head>
 
-<svelte:body on:contextmenu|preventDefault={handle_right_click} />
+<main-container class="flex flex-col h-full">
+    <topbar-container class="h-8">
+        {#if show_topbar}
+            <Topbar />
+        {/if}
+    </topbar-container>
 
-{#if show_main_context_menu}
-    <MainContextMenu
-        {...context_pos}
-        on:mousedown={close_context_menu}
-        on:clickoutside={close_context_menu}
-    />
-{/if}
-
-{#if show_topbar}
-    <Topbar />
-{/if}
+    <center-body class="flex h-full w-full">
+        
+    </center-body>
+</main-container>
