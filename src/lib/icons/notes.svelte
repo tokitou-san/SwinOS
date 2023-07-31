37c82c2b675167011044fb7177d4d5faa6418890
tokitou-san <tokitousan2005@gmail.com>
2023-07-31 13:09:00 +0530

    Create notes.svelte

---
+++ src/lib/icons/notes.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+    import type { SVGAttributes } from "svelte/elements";
+    type $$Props = SVGAttributes<SVGElement>;
+</script>
+
+<svg
+    {...$$props}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M28.8889 40H2.22222C0.994444 40 0 39.0056 0 37.7778V2.22222C0 0.994444 0.994444 0 2.22222 0H37.7778C39.0056 0 40 0.994444 40 2.22222V28.8889L28.8889 40Z"
+        fill="url(#paint0_linear_22_435)"
+    />
+    <path
+        d="M37.7778 40H20L40 20V37.7778C40 39.0056 39.0056 40 37.7778 40Z"
+        fill="url(#paint1_linear_22_435)"
+    />
+    <path
+        d="M22.2222 20H40L20 40V22.2222C20 20.9944 20.9944 20 22.2222 20Z"
+        fill="url(#paint2_linear_22_435)"
+    />
+    <defs>
+        <linearGradient
+            id="paint0_linear_22_435"
+            x1="1.07556"
+            y1="1.07556"
+            x2="34.38"
+            y2="34.38"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color="#FEDE00" />
+            <stop
+                offset="1"
+                stop-color="#FFD000"
+            />
+        </linearGradient>
+        <linearGradient
+            id="paint1_linear_22_435"
+            x1="30.2222"
+            y1="30.2222"
+            x2="37.7556"
+            y2="37.7556"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color="#E5A505" />
+            <stop
+                offset="0.116"
+                stop-color="#EAA804"
+            />
+            <stop
+                offset="0.595"
+                stop-color="#F8B301"
+            />
+            <stop
+                offset="1"
+                stop-color="#FDB700"
+            />
+        </linearGradient>
+        <linearGradient
+            id="paint2_linear_22_435"
+            x1="20.6611"
+            y1="20.6611"
+            x2="30.2711"
+            y2="30.2711"
+            gradientUnits="userSpaceOnUse"
+        >
+            <stop stop-color="#4B4B4B" />
+            <stop
+                offset="1"
+                stop-color="#3B3B3B"
+            />
+        </linearGradient>
+    </defs>
+</svg>
