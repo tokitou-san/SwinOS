2b12e4b5cba3366c7f34660d6fcae6acb537be55
tokitou-san <tokitousan2005@gmail.com>
2023-07-31 13:09:04 +0530

    Create terminal.svelte

---
+++ src/lib/icons/terminal.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { SVGAttributes } from "svelte/elements";
+    type $$Props = SVGAttributes<SVGElement>;
+</script>
+
+<svg
+    {...$$props}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        d="M3.63636 40C1.628 40 0 38.1357 0 35.8571V21.3571V17.2143V11H40V17.2143V21.3571V35.8571C40 38.1357 38.3727 40 36.3636 40H20H3.63636Z"
+        fill="#E1E1E1"
+    />
+    <path
+        d="M5 5V7.39124L9 12.6087L5 17.6085V20L11 12.6087L5 5Z"
+        fill="#CEC5DC"
+    />
+    <path
+        d="M3.63636 0C1.628 0 0 1.91871 0 4.28571V19.2857V23.5714V30H40V23.5714V19.2857V4.28571C40 1.91871 38.3727 0 36.3636 0H20H3.63636Z"
+        fill="#E1E1E1"
+    />
+    <path
+        d="M6.31543 6.74554V9.11397L12.6312 12.7982L6.31543 16.4824V18.8508L16.8417 12.7982L6.31543 6.74554ZM16.8417 17.0087V19.114H27.3681V17.0087H16.8417Z"
+        fill="#3F3D56"
+    />
+</svg>
