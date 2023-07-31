194975b06c0a7c507b9e7e42d476d590091454b5
tokitou-san <tokitousan2005@gmail.com>
2023-08-01 05:16:02 +0530

    feat: add icons on app context menu

--- src/lib/components/contextmenus/app_popdown_context_menu.svelte
+++ src/lib/components/contextmenus/app_popdown_context_menu.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import Open from "$lib/icons/open.svelte";
+    import Pin from "$lib/icons/pin.svelte";
     import { clickoutside } from "$lib/utils/clickoutside";
     import { createEventDispatcher } from "svelte";
+    import type { SvelteComponentDev } from "svelte/internal";
     import { fade } from "svelte/transition";
 
     export let x: number;
@@ -11,6 +14,23 @@
     function handle_outside_click() {
         dispatch("clickoutside");
     }
+
+    /* Menu options mappings */
+    const menu_mapping: {
+        [key: string]: {
+            icon: typeof SvelteComponentDev;
+            text: string;
+        };
+    } = {
+        open: {
+            icon: Open,
+            text: "Open"
+        },
+        pin: {
+            icon: Pin,
+            text: "Pin to taskbar"
+        }
+    };
 </script>
 
 <context-menu
@@ -21,8 +41,16 @@
     class="absolute z-[999] block h-max w-40 overflow-hidden rounded-md bg-white/20 leading-none text-white drop-shadow-2xl backdrop-blur-xl"
     style="top: {y}px; left: {x}px;"
 >
-    <button class="w-full px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">
-        Open <span class="font-semibold capitalize">{app_name}</span>
-    </button>
-    <button class="w-full px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">Pin to taskbar</button>
+    {#each Object.entries(menu_mapping) as item}
+        {@const component = item[1].icon}
+        {@const text = item[1].text}
+
+        <button class="flex w-full items-center gap-2 px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">
+            <svelte:component
+                this={component}
+                class="w-4 opacity-75"
+            />
+            <span>{text}</span>
+        </button>
+    {/each}
 </context-menu>
