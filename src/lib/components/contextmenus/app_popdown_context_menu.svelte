8c1b53fe3b9d85583a615d709bebbf868272ffdc
tokitou-san <tokitousan2005@gmail.com>
2023-07-31 18:00:56 +0530

    style: add dropshadow to apps context menu

--- src/lib/components/contextmenus/app_popdown_context_menu.svelte
+++ src/lib/components/contextmenus/app_popdown_context_menu.svelte
@@ -17,7 +17,7 @@
     on:mousedown
     transition:fade={{ duration: 50 }}
     use:clickoutside={handle_outside_click}
-    class="absolute z-[999] block h-max w-40 overflow-hidden rounded-md bg-white/20 leading-none text-white backdrop-blur-xl"
+    class="absolute z-[999] block h-max w-40 overflow-hidden rounded-md bg-white/20 leading-none text-white drop-shadow-2xl backdrop-blur-xl"
     style="top: {y}px; left: {x}px;"
 >
     <button class="w-full px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">
