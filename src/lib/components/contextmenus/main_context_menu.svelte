7f6be2474a0cfd533921a4fe40f2ce17397c49df
tokitou-san <tokitousan2005@gmail.com>
2023-07-31 21:24:01 +0530

    feat: add main context menu [partially]

---
+++ src/lib/components/contextmenus/main_context_menu.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { clickoutside } from "$lib/utils/clickoutside";
+    import { createEventDispatcher } from "svelte";
+    import { fade } from "svelte/transition";
+
+    export let x: number;
+    export let y: number;
+
+    const dispatch = createEventDispatcher();
+    function handle_outside_click() {
+        dispatch("clickoutside");
+    }
+</script>
+<context-menu
+    on:mousedown
+    transition:fade={{ duration: 50 }}
+    use:clickoutside={handle_outside_click}
+    class="absolute z-[999] block h-max w-48 overflow-hidden rounded-md bg-white/20 leading-none text-white drop-shadow-2xl backdrop-blur-xl"
+    style="top: {y}px; left: {x}px;"
+>
+    <button class="w-full px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">
+        Refresh
+    </button>
+    <button class="w-full px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">
+        Change background
+    </button>
+    <button class="w-full px-3 py-2 text-left text-xs transition-colors hover:bg-white/10">Settings</button>
+</context-menu>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import Topbar from "$lib/components/topbar.svelte";
+    import MainContextMenu from "$lib/components/contextmenus/main_context_menu.svelte";
+import Topbar from "$lib/components/topbar.svelte";
 
     const SHOW_COMPONENTS_DELAY = 300;
     // show / hide components
@@ -8,11 +9,31 @@
     setTimeout(() => {
         show_topbar = true;
     }, SHOW_COMPONENTS_DELAY);
+
+    /* Context menu */
+    let show_main_context_menu = false;
+    let context_pos = { x: 0, y: 0 };
+
+    function handle_right_click(event: MouseEvent) {
+        // show/hide custom menu
+        show_main_context_menu = !show_main_context_menu;
+        // set event position
+        context_pos = { x: event.clientX, y: event.clientY };
+    }
+    function close_context_menu() {
+        show_main_context_menu = false;
+    }
 </script>
 
 <svelte:head>
     <title>Swin OS</title>
 </svelte:head>
+
+<svelte:body on:contextmenu|preventDefault={handle_right_click} />
+
+{#if show_main_context_menu}
+    <MainContextMenu {...context_pos} on:mousedown={close_context_menu} on:clickoutside={close_context_menu} />
+{/if}
 
 {#if show_topbar}
     <Topbar />
