7b040bb771c2c46354abeeb36746517358372e15
tokitou-san <tokitousan2005@gmail.com>
2023-08-13 15:59:11 +0530

    remove console.log

--- src/lib/utils/clickoutside.ts
+++ src/lib/utils/clickoutside.ts
@@ -3,7 +3,6 @@ import { onMount } from "svelte";
 export function clickoutside(node: HTMLElement, options: { callback_function: () => void; exclude: Array<HTMLElement> }) {
     function on_click(event: MouseEvent) {
         if (!node.contains(event.target as HTMLElement) && !options.exclude.some((el) => el && el.contains(event.target as HTMLElement)) && typeof options.callback_function === "function") {
-            console.log(options.exclude);
             options.callback_function();
         }
     }
